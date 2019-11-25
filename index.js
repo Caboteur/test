c12bdd9a9dcac2aeae68f36d7d5219dfa356db08
Adrien BLANCHOT <caboteur.adrienbanchot@gmail.com>
2019-11-25 18:19:54 +0100

    tsk

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ jQuery(document).ready(function ($) {
 
     var BtnRight = $('.sl-arr-right');
     var BtnLeft = $('.sl-arr-left');
+    var side = 1;
+    var width = 0;
+    var isRunning = false;
 
     function SlideRight(){
-      $( ".sl-first-slide" ).replaceWith( '<div class="sl-second-slide sl-slide-appear"><div class="sl-second-slide-wrapper"><div class="sl-info"><h2>PHOTO REPORTER</h2><h1>PHOTO-JOURNALISTE</h1><hr class="sl-hr"></hr></br><p>Photographe corporate, je traduis efficacement et esthétiquement en images les messages et les idées des entreprises à travers des portraits de leurs collaborateurs et de leurs dirigeants. Jinterviens au sein de lentreprise ou lors dévénements pour des portraits déquipe comme pour des photos individuelles. Je pratique la <b>photographie en entreprise</b> depuis plus de 20 ans et je suis basé en région parisienne</p><a>DECOUVRIR MES PORTRAITS CORPORTATE</a></div><div class="sl-background-img alt="photo reporter"></div></div></div>' );
-
+       $('.sl-first-slide').css({display:'none'});
+       $('.sl-second-slide').css({display:'inherit'});
+       update();
     }
 
     function SlideLeft(){
-      $( ".sl-second-slide" ).replaceWith( '   <div class="sl-first-slide sl-slide-appear"><li><div class="sl-column"><h2>PHOTOGRAPHE PROFESSIONNEL A PARIS</h2><h1>PHOTOGRAPHE CORPORATE</h1><p>Photographe corporate, je traduis efficacement et esthétiquement en images les messages et les idées des entreprises à travers des portraits de leurs collaborateurs et de leurs dirigeants. Jinterviens au sein de lentreprise ou lors dévénements pour des portraits déquipe comme pour des photos individuelles. Je pratique la <b>photographie en entreprise</b> depuis plus de 20 ans et je suis basé en région parisienne</p><a>DECOUVRIR MES PORTRAITS CORPORTATE</a></div></li><li><div class="sl-column"><img class="sl-profil-img" src="./sportrait.jpg"></img></div></li></div>')
+      $('.sl-first-slide').css({display:'grid'});
+      $('.sl-second-slide').css({display:'none'});
+      update();
     }
 
 
@@ -74,32 +80,38 @@ jQuery(document).ready(function ($) {
   });
 
   function update() {
+    console.log(isRunning)
+    width = 0;
 
-    var width = 0;
     var identity = setInterval(scene, 80);
+      if (isRunning == true){
+          clearInterval(identity);
+      }
     function scene() {
-      if (width >= 100) {
+      if (width >= 100 ) {
+        isRunning = false;
         clearInterval(identity);
+        if (side == 1){
+           side = 2;
+           SlideRight();
+         } else if (side == 2) {
+           side = 1;
+           SlideLeft();
+         }
+
       } else {
+        isRunning = true;
+
         width++ ;
           $('.sl-progress').css({width:width + '%'});
+
       }
     }
   }
   update();
 
 
-  setInterval(function(){
-    update();
-  }, 8000);
-  setInterval(function(){
-    SlideRight();
-  }, 8000);
-  setInterval(function(){
-    SlideLeft();
-  }, 16000);
 
   var hauteurTotaleElem = document.getElementById("sl-container").offsetHeight;
-  console.log(hauteurTotaleElem);
   $('#sl-container').css({height:hauteurTotaleElem});
 });
